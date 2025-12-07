921f426f0f894c81e92a03c29624341b512b0ff2
IvanRusenov <114363124+IvanRusenov@users.noreply.github.com>
2025-12-07 18:23:48 +0200

    fmt files

--- variables.tf
+++ variables.tf
@@ -1,95 +1,95 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  type = string
+  type        = string
 }
 
 variable "resource_group_location" {
   description = "Azure region to deploy resources"
-  type = string
+  type        = string
 }
 
 variable "app_service_plan_name" {
   description = "The name of the app service plan"
-  type = string
+  type        = string
 }
 
 variable "web_app_name" {
   description = "The name of the app service"
-  type = string
+  type        = string
 }
 
 variable "sql_server_name" {
   description = "The name of the sql server"
-  type = string
+  type        = string
 }
 
 variable "sql_database_name" {
   description = "The name of the database"
-  type = string
+  type        = string
 }
 
 variable "database_collation" {
   description = "The Collation of the database"
-  type = string
+  type        = string
 }
 
 variable "sql_admin_login" {
-    description = "The admin username"
+  description = "The admin username"
 }
 
 variable "sql_admin_login_password" {
   description = "The admin password"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "firewall_rule_name" {
   description = "The name of the SQL Server firewall rule"
-  type = string
+  type        = string
 }
 
 variable "source_control_url" {
   description = "The url of the github repo"
-  type = string
+  type        = string
 }
 
 variable "subscription_id" {
   description = "The id of the azure account"
-  type = string
+  type        = string
 }
 
 variable "os_type" {
   description = "The type of operating system"
-  type = string
+  type        = string
 }
 
 variable "service_plan_sku_name" {
   description = "The service plan sku name"
-  type = string
+  type        = string
 }
 
 variable "database_sku_name" {
   description = "The database sku name"
-  type = string
+  type        = string
 }
 
 variable "database_zone_redundant" {
   description = "Is zone redundant enable"
-  type = bool
+  type        = bool
 }
 
 variable "database_storage_account_type" {
   description = "The type of storage account(Local)"
-  type = string
+  type        = string
 }
 
 variable "database_prevent_destroy" {
   description = "Prevent database from destroing(true,false)"
-  type = bool
+  type        = bool
 }
 
 variable "web_app_database_connection_string" {
   description = "Connection string"
-  type = string
+  type        = string
 }
 
